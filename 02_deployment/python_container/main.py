b6c57155422e29359070ec12b01ead0003221665
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-09-21 09:33:29 -0300

    initial project

---
+++ 02_deployment/python_container/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def hello(path):
+    return "[v1] Hello, Kubernetes!\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4567)
+
