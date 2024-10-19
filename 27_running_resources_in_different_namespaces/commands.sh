6ea01687ad1b5e930264ce25bf6e6a0802370cac
lewisaudeone <80711658+lewisc99@users.noreply.github.com>
2024-10-19 15:08:42 -0300

    created 27_running_resources_in_different_namespaces v2

--- 27_running_resources_in_different_namespaces/commands.sh
+++ 27_running_resources_in_different_namespaces/commands.sh
@@ -5,10 +5,14 @@ kubectl apply -f nginx-pod.yaml
 kubectl apply -f nginx-pod.yaml -n my-namespace
 # pod/nginx created
 
+#if we have the namespace in the nginx-pod add the command below
+kubectl apply -f nginx-pod.yaml
+
 kubectl get pods
 # NAME    READY   STATUS 
 # nginx   1/1     Running
 
 kubectl get pods -n my-namespace
+
 # NAME    READY   STATUS   
 # nginx   1/1     Running 
